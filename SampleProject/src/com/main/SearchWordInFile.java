774531deedced214d2878580d33de60d954f4c0b
ShilpaIP <ShilpaIP@github.com>
2015-08-30 02:11:59 -0700

    Search Word and count occurence of it Program

---
+++ SampleProject/src/com/main/SearchWordInFile.java
@@ -0,0 +1,40 @@
+package com.main;
+
+import java.io.*;
+
+public class SearchWordInFile {
+	
+	public static int searchWord(String file, String searchKey) throws FileNotFoundException, IOException  {
+		
+		BufferedReader br = new BufferedReader(new FileReader(new File(file)));
+		String line = "";
+		int count = 0;
+		
+		while((line=br.readLine()) != null){
+			String[] splitstr = line.split(" ");
+			for(String s : splitstr){
+				if(s.equals(searchKey))
+					count++;
+			}
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		
+		String file = "SampleData.txt";
+		String wordToBeSearched = "Java";
+	
+		int searchWrdCount = 0;
+		try {
+			searchWrdCount = searchWord(file, wordToBeSearched);
+		} catch(FileNotFoundException e){
+			System.out.println("File Not Found");
+		} catch (IOException e) {		
+			e.printStackTrace();
+		}
+		System.out.println("Count of word" + " '"+ wordToBeSearched + "'" + " is: " + searchWrdCount);
+		
+	}
+	
+}
