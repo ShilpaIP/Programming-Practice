bc7f7b266f493c46255741b52144e5997d447beb
ShilpaIP <ShilpaIP@github.com>
2015-08-29 22:32:33 -0700

    Printing all elements of Enum class containing Months

---
+++ SampleProject/src/com/main/Months.java
@@ -0,0 +1,19 @@
+package com.main;
+
+public enum Months {
+	
+	JANUARY,
+	FEBRUARY,
+	MARCH,
+	APRIL,
+	MAY,
+	JUNE,
+	JULY,
+	AUGUST,
+	SEPTEMBER,
+	OCTOBER,
+	NOVEMBER,
+	DECEMBER
+	
+	
+}
---
+++ SampleProject/src/com/main/PrintMonths.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import java.util.Arrays;
+
+
+public class PrintMonths {
+	
+	public static void main(String[] args) {
+			
+		System.out.println(Arrays.asList(Months.values()));
+	}
+
+}
