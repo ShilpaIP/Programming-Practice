a7fb3c80ba6c7cac1bc756b430b120fc8d44b6c4
ShilpaIP <ShilpaIP@github.com>
2015-08-30 01:49:15 -0700

    Count words in file program

---
+++ SampleProject/src/com/main/CountWordInFile.java
@@ -0,0 +1,34 @@
+package com.main;
+
+import java.io.*;
+
+public class CountWordInFile {
+	
+	public static int countWords(String file) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(new File(file)));
+		String line = "";
+		int count = 0;
+	
+		while((line=br.readLine()) != null){
+			String[] splitstr = line.split(" ");
+			for(String s : splitstr){
+				count++;
+			}			
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		
+		String file = "SampleData.txt";
+		int wordCount = 0;
+		try {
+			wordCount = countWords(file);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		System.out.println(wordCount);
+	}
+
+}
